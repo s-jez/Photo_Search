5abe97efe23692ba7b157892467531ef67b88129
s-jez <stanislawjez7@gmail.com>
2022-10-02 22:10:40 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Unsplash</h1>
+      <p>
+        The internet's source of <a href="/">freely-usable images</a>
+      </p>
+      <p>Powered by creators everywhere.</p>
     </div>
   );
 }
