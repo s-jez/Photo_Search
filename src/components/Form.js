8171f8b924d67e8d3a33d731f414600c03ef7526
s-jez <stanislawjez7@gmail.com>
2022-10-06 21:11:19 +0200

    Add suggestions.

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,26 +7,31 @@ const Form = () => {
   const [suggestions, setSuggestions] = useState([]);
   const [data, setData] = useState([]);
 
-  const SEARCH_PHOTOS_URL = `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_API_KEY}&query=${inputValue}`;
-
+  let SEARCH_PHOTOS_URL = `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_API_KEY}&query=${inputValue}`;
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(SEARCH_PHOTOS_URL);
       const data = await res.json();
       setData(data.results);
+      console.log(data);
     };
     fetchData();
   }, [SEARCH_PHOTOS_URL]);
   const inputChangeHandler = (e) => {
-    setInputValue(e.target.value);
+    SEARCH_PHOTOS_URL += e.target.value;
     let matches = [];
     if (inputValue.length >= 3) {
       matches = data.filter((searchVal) => {
         const regex = new RegExp(`${inputValue}`, "gi");
-        return searchVal.match(regex);
+        return searchVal.tags[0]?.title?.match(regex);
       });
-      setSuggestions(matches);
     }
+    setSuggestions(matches);
+    setInputValue(e.target.value);
+  };
+  const onSuggestHandler = (text) => {
+    setInputValue(text);
+    setSuggestions([]);
   };
   return (
     <form className={styles["form-input"]}>
@@ -41,7 +46,15 @@ const Form = () => {
         placeholder="Search free high-resolution photos"
       />
       {suggestions &&
-        suggestions.map((suggestion, i) => <div>{suggestion}</div>)}
+        suggestions.map((suggestion, i) => (
+          <div
+            className={styles["input-suggestion"]}
+            key={i}
+            onClick={() => onSuggestHandler(suggestion)}
+          >
+            {suggestion}
+          </div>
+        ))}
     </form>
   );
 };
