50adee657b416f6d92a86dae5ea0e9fb99144b15
s-jez <stanislawjez7@gmail.com>
2022-10-17 21:16:48 +0200

    Add fetch to function.

--- src/components/Photos/PhotoForm.js
+++ src/components/Photos/PhotoForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InputText from "../InputText";
 import styles from "../../styles/InputText.module.css";
 import PhotoGallery from "./PhotoGallery";
@@ -6,10 +6,22 @@ import formStyles from "../../styles/Form.module.css";
 
 const PhotoForm = () => {
   const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState([]);
+  let SEARCH_PHOTOS_URL = `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_API_KEY}&query=${inputValue}`;
   const inputChangeHandler = (ev) => {
     setInputValue(ev.target.value);
   };
   const formSubmitHandler = () => {};
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(SEARCH_PHOTOS_URL);
+      const data = await res.json();
+      setData(data.results);
+      console.log(data);
+    };
+    fetchData();
+  }, [SEARCH_PHOTOS_URL]);
   return (
     <>
       <form onSubmit={formSubmitHandler}>
