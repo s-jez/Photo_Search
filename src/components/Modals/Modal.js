35114f88c7a68e97a53fedb9de95113d505761be
s-jez <stanislawjez7@gmail.com>
2022-10-18 10:18:27 +0200

    Add props to Modal.js

--- src/components/Modals/Modal.js
+++ src/components/Modals/Modal.js
@@ -1,3 +1,12 @@
-const Modal = () => {
+const Modal = (props) => {
+    const {handleClose, show, children} = props;
+    return (
+        <div>
+            <div>{children}</div>
+            <div>
+                <button onClick={handleClose}>Close</button>
+            </div>
+        </div>
+    )
 }
 export default Modal
