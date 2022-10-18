142374d26d365da00b8ee884af06145293262531
s-jez <stanislawjez7@gmail.com>
2022-10-18 17:29:43 +0200

    Add font to GalleryContent

--- src/pages/GalleryContent.js
+++ src/pages/GalleryContent.js
@@ -1,9 +1,11 @@
 import FormPhoto from "../../src/components/Form/FormPhoto";
 import PhotoGallery from "../../src/components/Photos/PhotoGallery";
+import GlobalStyle from "../globalStyles";
 
 const GalleryContent = () => {
   return (
     <div>
+      <GlobalStyle />
       <FormPhoto />
       <PhotoGallery />
     </div>
