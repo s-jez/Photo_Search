8e4c38e54e4428574e4c495a64f3d2b6786605c9
s-jez <stanislawjez7@gmail.com>
2022-10-19 19:13:21 +0200

    add GetRandomPhoto.js function.

--- src/components/Photos/GetRandomPhoto.js
+++ src/components/Photos/GetRandomPhoto.js
@@ -1,10 +1,18 @@
 let UNSPLASH_URL = `https://api.unsplash.com`;
 let UNSPLASH_KEY = `?client_id=${process.env.REACT_APP_API_KEY}`;
 
-
-const GetRandomPhoto = () => {
-    fetch(UNSPLASH_URL+"/photos/random" + UNSPLASH_KEY)
-    .then(response => response.json())
-    .then(data => console.log(data))
-}
-export default GetRandomPhoto;
+let randomPhoto;
+const getRandomPhoto = () => {
+  fetch(UNSPLASH_URL + "/photos/random" + UNSPLASH_KEY)
+    .then((response) => response.json())
+    .then((data) => {
+      randomPhoto = data.urls.full;
+    });
+  console.log(randomPhoto);
+  if (randomPhoto === undefined) {
+    return "https://i.imgur.com/O0eAkcG.jpg";
+  } else {
+    return randomPhoto;
+  }
+};
+export default getRandomPhoto;
--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,23 +1,32 @@
 import React from "react";
 import Form from "../components/Form/Form";
 import styles from "../../src/App.module.css";
-import GetRandomPhoto from "../components/Photos/GetRandomPhoto";
+import getRandomPhoto from "../components/Photos/getRandomPhoto";
+import { useEffect, useState } from "react";
 
 const MainContent = () => {
+  const [randomPhoto, setRandomPhoto] = useState("");
+  useEffect(() => {
+    setRandomPhoto(getRandomPhoto);
+  }, [randomPhoto]);
   return (
     <>
-      <GetRandomPhoto/>
       <div className={styles.App}>
-        <div className={styles["App__card"]}>
-          <h1>Unsplash</h1>
-          <p>
-            The internet's source of{" "}
-            <a href="https://unsplash.com/" target="_blank" rel="noreferrer">
-              freely-usable images
-            </a>
-          </p>
-          <p>Powered by creators everywhere.</p>
-          <Form />
+        <div
+          className={styles["App__card"]}
+          style={{ backgroundImage: `url(${randomPhoto})` }}
+        >
+          <div className={styles["Card"]}>
+            <h1>Unsplash</h1>
+            <p>
+              The internet's source of{" "}
+              <a href="https://unsplash.com/" target="_blank" rel="noreferrer">
+                freely-usable images
+              </a>
+            </p>
+            <p>Powered by creators everywhere.</p>
+            <Form />
+          </div>
         </div>
       </div>
     </>
