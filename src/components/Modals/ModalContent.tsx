24b528a23107d8116c00c8e0486fda95c85f7249
s-jez <stanislawjez7@gmail.com>
2022-11-01 18:21:11 +0100

    Add to modal overlay styles.

--- src/components/Modals/ModalContent.tsx
+++ src/components/Modals/ModalContent.tsx
@@ -22,31 +22,32 @@ export type ModalProps = {
 const ModalContent = ({ handleClose, data }: ModalProps) => {
   // eslint-disable-next-line
   return (
-    <div className="modal">
-      <span onClick={handleClose} className="cursor-pointer">
-        <AiOutlineClose size={22} />
-      </span>
-
-      <div style={{ position: "relative", height: "auto" }}>
-        <div className="photo__modal--avatar">
-          <img src={data.user.profile_image.small} alt="" />
-          <span>{data.user.username}</span>
-        </div>
-        <div className="photo__modal">
-          <img
-            src={data.urls.full}
-            alt={data.alt_description}
-            width={600}
-            height={600}
-            style={{ objectFit: "cover" }}
-          />
-        </div>
-        <div className="photo__modal--likes">
-          <span>Likes</span>
-          <p>{data.likes}</p>
-          <a href={data.urls.full} target="_blank" rel="noreferrer" download>
-            Download
-          </a>
+    <div className="modal-overlay" onClick={handleClose}>
+      <div className="modal">
+        <div className="relative">
+          <button className="cursor-pointer">
+            <AiOutlineClose size={22} className="mx-5" />
+          </button>
+          <div className="photo__modal--avatar">
+            <img src={data.user.profile_image.small} alt="" />
+            <span>{data.user.username}</span>
+          </div>
+          <div className="photo__modal">
+            <img
+              src={data.urls.full}
+              alt={data.alt_description}
+              width={600}
+              height={600}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className="photo__modal--likes">
+            <span>Likes</span>
+            <p>{data.likes}</p>
+            <a href={data.urls.full} target="_blank" rel="noreferrer" download>
+              Download
+            </a>
+          </div>
         </div>
       </div>
     </div>
